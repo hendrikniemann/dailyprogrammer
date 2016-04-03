b6cc925c7f35b79d57e9bca59a7e326b867cb825
hendrikniemann <h.neverman@web.de>
2016-04-03 23:27:22 +0200

    switch to ava for intermediate #259

--- intermediate/c259/spec.js
+++ intermediate/c259/spec.js
@@ -1,30 +1,25 @@
-import 'should';
+import test from 'ava';
 
 import challenge259 from '.';
 
 // https://www.reddit.com/4bmdwz
-describe('Challenge #259 [Intermediate] Mahjong Hands', () => {
-  it('should solve the first sample input', () => {
-    const input = `14
+test('Challenge #259 [Intermediate] Mahjong Hands', t => {
+  let input;
+  input = `14
 Circle,4\nCircle,5\nCircle,6\nBamboo,1\nBamboo,2\nBamboo,3\nCharacter,2
 Character,2\nCharacter,2\nCircle,1\nCircle,1\nBamboo,7\nBamboo,8\nBamboo,9`;
 
-    challenge259(input).should.eql('Winning hand');
-  });
+  t.true(challenge259(input) === 'Winning hand', 'First example input');
 
-  it('should solve the second sample input', () => {
-    const input = `14
+  input = `14
 Circle,4\nBamboo,1\nCircle,5\nBamboo,2\nCharacter,2\nBamboo,3\nCharacter,2\nCircle,6\nCharacter,2
 Circle,1\nBamboo,8\nCircle,1\nBamboo,7\nBamboo,9`;
 
-    challenge259(input).should.eql('Winning hand');
-  });
+  t.true(challenge259(input) === 'Winning hand', 'Second example input');
 
-  it('should solve the third sample input', () => {
-    const input = `14
+  input = `14
 Circle,4\nCircle,5\nCircle,6\nCircle,4\nCircle,5\nCircle,6\nCircle,1\nCircle,1\nBamboo,7\nBamboo,8
 Bamboo,9\nCircle,4\nCircle,5\nCircle,6`;
 
-    challenge259(input).should.eql('Winning hand');
-  });
+  t.true(challenge259(input) === 'Winning hand', 'Third example input');
 });

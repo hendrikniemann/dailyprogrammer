6de52d3ae9467e711b9912b62bdd65b16b40a0d0
hendrikniemann <h.neverman@web.de>
2016-04-27 12:00:45 +0200

    cleanup tests

--- hard/c259/spec.js
+++ hard/c259/spec.js
@@ -4,5 +4,7 @@ import challenge, { readNos } from '.';
 
 test('Challenge #259 [Hard] Diagonal collision', t => {
   t.true(readNos('10020') === 21);
-  console.log(challenge(7));
+  const res = challenge(7);
+
+  t.true(res[21] === '10020');
 });

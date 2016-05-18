046b4a46d447bebdab5f3dba4ce5ffd41eb694ac
hendrikniemann <h.neverman@web.de>
2016-05-18 23:47:07 +0200

    add tests for makeNth 11, 12, 13

--- easy/c267/test.js
+++ easy/c267/test.js
@@ -20,6 +20,9 @@ test('makeNth', t => {
   t.true(makeNth(2) === '2nd');
   t.true(makeNth(3) === '3rd');
   t.true(makeNth(4) === '4th');
+  t.true(makeNth(11) === '11th');
+  t.true(makeNth(12) === '12th');
+  t.true(makeNth(13) === '13th');
   t.true(makeNth(101) === '101st');
   t.true(makeNth(672) === '672nd');
   t.true(makeNth(8490563) === '8490563rd');
